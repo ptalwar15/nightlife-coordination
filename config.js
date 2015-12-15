c88c5a80b2643579a4de21a19eec474a3573c079
Pradeep Talwar <ptal528@yahoo.com>
2015-12-14 19:30:40 -0500

    Added mongolab db 2 for site

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'port': process.env.PORT || 3000,
-	'database':mongodb://ptalwar15:Coolit12@ds057214.mongolab.com:57214/nightlife,
+	'database':"mongodb://ptalwar15:Coolit12@ds057214.mongolab.com:57214/nightlife",
 	'secret':'ilovescotchscotchyscotchscotch'
 }
 
